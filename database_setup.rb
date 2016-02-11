e1c52a3f17e35d513747cb1ce0ba62b2676a4bb1
BakaBBQ <liubaqiao@126.com>
2016-02-11 15:44:35 +0800

    maybe I am doing this all wrong

--- database_setup.rb
+++ database_setup.rb
@@ -1,18 +1,11 @@
 require "data_mapper"
 
-env = nil
-
-if ENV["bakak_env"]
-  env = ENV["bakak_env"]
+if ENV["CI"]
+  puts "Loading From CI"
+  DataMapper::Logger.new($stdout, :debug)
+  DataMapper.setup(:default, 'sqlite::memory:')
 else
-  env = "local"
-end
-
-case env
-  when "local"
-    DataMapper::Logger.new($stdout, :debug)
-    DataMapper.setup(:default, 'sqlite://./project.db')
-  when "ci"
-    DataMapper::Logger.new($stdout, :debug)
-    DataMapper.setup(:default, 'sqlite::memory:')
+  puts "Loading from Local"
+  DataMapper::Logger.new($stdout, :debug)
+  DataMapper.setup(:default, 'sqlite://./project.db')
 end
