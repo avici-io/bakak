104e41486bdf5d09034f1a311b4723dae024d211
BakaBBQ <liubaqiao@126.com>
2016-02-11 15:49:29 +0800

    I am getting crazy

--- database_setup.rb
+++ database_setup.rb
@@ -3,7 +3,7 @@ require "data_mapper"
 if ENV["CI"]
   puts "Loading From CI"
   DataMapper::Logger.new($stdout, :debug)
-  DataMapper.setup(:default, 'sqlite::memory:')
+  DataMapper.setup(:default, 'sqlite:///project.db')
 else
   puts "Loading from Local"
   DataMapper::Logger.new($stdout, :debug)
