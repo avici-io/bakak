42542392aabc258781b17056ca3e8c8583e552f1
BakaBBQ <liubaqiao@126.com>
2016-02-14 15:43:16 +0800

    okay heroku should work

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 #
-CONFIG = {
+config = {
   :token_key => "Rb-5qB56khXJYYWNlak_9cPOBwYCopIDh-u8RTzOCJYJJdURF7PSZc8XkMsEY2hXS8Sz",
   :mailgun => ENV["MAIGUN_KEY"],
   :qiniu => {
@@ -13,3 +13,9 @@ CONFIG = {
       db: ENV["REDIS_DB"].to_i
   }
 }
+
+if ENV["REDIS_PASSWORD"]
+  config[:redis][:password] = ENV["REDIS_PASSWORD"]
+end
+
+CONFIG = config
